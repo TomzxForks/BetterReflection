47fbef70550682b8fdb5f3b69f34f39accb37fdb
James Titcumb <james@asgrim.com>
2016-10-07 16:28:29 +0300

    Updated docs and demo with new example API

--- demo/monkey-patching/index.php
+++ demo/monkey-patching/index.php
@@ -4,13 +4,15 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 
 use BetterReflection\Reflector\ClassReflector;
 use BetterReflection\SourceLocator\Type\SingleFileSourceLocator;
-use BetterReflection\Util\Autoload;
+use BetterReflection\Util\Autoload\ClassLoader;
+use BetterReflection\Util\Autoload\ClassLoaderMethod\EvalLoader;
 
-Autoload::initialise();
+$loader = new ClassLoader(new EvalLoader());
+$loader->register();
 
 // Create the reflection first (without loading)
 $classInfo = (new ClassReflector(new SingleFileSourceLocator(__DIR__ . '/MyClass.php')))->reflect('MyClass');
-Autoload::addClass($classInfo);
+$loader->addClass($classInfo);
 
 // Override the body...!
 $classInfo->getMethod('foo')->setBodyFromClosure(function () {
